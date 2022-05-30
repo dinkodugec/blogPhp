cdb716d806931395445edebb5413012839f045bf
Dinko <dinko.dugec@gmail.com>
2022-05-30 20:10:44 +0200

    designing my profile page

---
+++ myProfile.php
@@ -0,0 +1,273 @@
+<?php require_once("Includes/db.php") ;?>
+<?php require_once("Includes/functions.php") ;?>
+<?php require_once("Includes/sessions.php") ;?>
+     <?php  
+      $_SESSION["trackingURL"]=$_SERVER["PHP_SELF"];
+     confirmLogin();   
+    ?>
+
+<?php 
+/* fetching existing admin data start */
+    $adminId = $_SESSION['userId'];
+    global $connectingDB;
+    $sql = "SELECT * FROM admins WHERE id='$adminId'";
+    $stmt = $connectingDB->query($sql);
+    while($dataRows = $stmt->fetch()){
+      $existingName = $dataRows['aname'];
+    }
+    /* fetching existing admin data end */
+
+    if(isset($_POST['submit'])){        /* this submit must match name which you gave this buttom */
+      $postTitle = $_POST['postTitle'];
+      $category=$_POST['category'];
+      $image = $_FILES['image']['name'];
+      $target = "Upload/".basename( $_FILES['image']['name']);
+      $postText = $_POST['postDescription'];
+      $admin = $_SESSION["username"];
+
+
+    date_default_timezone_set("Asia/Karachi");
+    $currentTime=time();
+    $dateTime=strftime("%B-%d-%Y %H:%M:%S", $currentTime);
+    
+
+
+     
+    if(empty($postTitle)){
+      $_SESSION['ErrorMesage'] = "Title can not be empty";
+      redirectTo("addNewPost.php");
+          }elseif(strlen($postTitle)<5){
+            $_SESSION['ErrorMesage'] = "Post title should be greather than two charachters ";
+            redirectTo("addNewPost.php");
+          }elseif(strlen($postTitle)>9999){   /* because we put in database varchar(50) */
+            $_SESSION['ErrorMesage'] = "Post description should be less than 1000 charachters ";
+            redirectTo("addNewPost.php");
+        }else{
+            //query to insert post in DB when everything is fine
+            global $connectingDB;
+            $sql = "INSERT INTO posts(title,category,author,image,post)";
+            $sql .= "VALUES(:postTitle,:categoryName,:adminName, :imageName,:postDescription)";
+            $stmt = $connectingDB->prepare($sql);
+            $stmt->bindValue(':postTitle',$postTitle);
+            $stmt->bindValue(':categoryName',$category);
+            $stmt->bindValue(':adminName', $admin);
+            $stmt->bindValue(':imageName', $image);
+            $stmt->bindValue(':postDescription',$postText);
+
+          /*   $stmt->bindValue(':dateTime',$datetime); */
+
+            $excute=$stmt->execute();
+
+            move_uploaded_file($_FILES['image']['tmp_name'], $target);
+
+            if($excute){
+              $_SESSION['SuccessMessage']="Post with id : ".$connectingDB->lastInsertId()."Added Successfully";
+              redirectTo('addNewPost.php');
+            }else{
+              $_SESSION['ErrorMesage'] = "It is wrong sometimes ";
+              redirectTo("addNewPost.html");
+            }
+        }
+
+}  /*  Ending of submit button if-condition */
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <script src="https://kit.fontawesome.com/7156340534.js" crossorigin="anonymous"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/css/bootstrap.min.css"
+      integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" href="css/styles.css">
+    <title>My profile</title>
+  </head>
+  <body>
+    <!--  <h1 class="display-1">Hello World</h1>
+    <h1 class="display-2">Hello World</h1>
+    <h1 class="display-3">Hello World</h1>
+    <h1 class="display-4">Hello World</h1>  ALL H1 Classes  -->
+
+   
+    
+    <!-- NAVBAR -->
+     <div style="height:10px; background:#27aae1;"></div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">   <!--  <nav class="navbar navbar-expand-lg bg-primary, bg-success"></nav> -->
+          <div class="container">    <!-- <div class="container" style="height: 30px; background:red;"> Just to see container style</div> -->
+            <a href="#" class="navbar-brand">Dinko Dugec</a>
+            <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarcollapseCMS">
+                  <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarcollapseCMS">
+            <ul class="navbar-nav mr-auto"> <!--  margin reight:auto -->
+                  <li class="nav-item">   
+                      <a href="myProfile.php" class="nav-link"><i class="fas fa-user text-success"></i>My Profile</a>  
+                 </li>
+                  <li class="nav-item">
+                        <a href="dashboard.php" class="nav-link">Dashboard</a>  
+                  </li>
+                  <li class="nav-item">
+                        <a href="posts.php" class="nav-link">Posts</a>  
+                  </li>
+                  <li class="nav-item">
+                        <a href="categories.php" class="nav-link">Categories</a>  
+                  </li>
+                  <li class="nav-item">
+                        <a href="admins.php" class="nav-link">Manage Admins</a>  
+                  </li>
+                  <li class="nav-item">
+                        <a href="comments.php" class="nav-link">Comments</a>  
+                  </li>
+                  <li class="nav-item">
+                        <a href="blog.php?page=1" class="nav-link">Live Blog</a>  
+                  </li>
+            </ul>
+          
+
+            <ul class="navbar-nav ml-auto">
+                 <li class="nav-item">
+                       <a href="logout.php" class="nav-link text-danger"><i class="fas fa-user-times ml-auto"></i>Logout</a>
+                 </li>   
+            </ul>
+          </div>
+          </div>
+    </nav>
+    <div style="height:10px; background:#27aae1;"></div>
+     </nav>
+     <!-- NAVBAR END -->
+
+     <!-- HEADER -->
+    <header class="bg-dark text-white py-4">    <!-- p means padding  -->
+      <div class="container">
+            <div class="row">
+                  <div class="col-md-12">
+                    <h1><i class="fas fa-user mr-2"></i>My Profile</h1>
+                  </div>
+            </div>
+      </div>
+    </header>
+    <!-- END OF HEADER -->
+
+       <!-- Main Area -->
+  
+       <section class="container py-2 mb-4">
+         <div class="row">
+           <!-- left area -->
+           <div class="col-md-3">
+             <div class="card">
+               <div class="card-header bg-dark text-light">
+                  <h3><?php  echo $existingName;  ?></h3>
+               </div>
+               <div class="card-body">
+                 <img src="images/zoe.jpg" class="block img-b-3" alt="">
+                 <div>
+                   Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam quod et enim commodi exercitationem expedita fuga quasi placeat, temporibus aspernatur, 
+                   hic quibusdam suscipit error vitae debitis vel reiciendis provident ratione.
+                 </div>
+               </div>
+             </div>
+           </div>
+       <!--     right Area -->
+           <div class="col-md-9" style="min-height:400px">
+                <?php echo ErrorMessage();
+                      echo SuccessMessage();
+                ?>
+              <form action="addNewPost.php" method="post" enctype="multipart/form-data">
+                <div class="card bg-secondary text-light mb-3">
+                  <div class="card-body bg-dark">
+                    <div class="form-group">
+                      <label for="title"><span class="fieldInfo">Post Title:</span> </label>
+                      <input class="form-control" type="text" name="postTitle" id="title" placeholder="Type title here" value="">
+                    </div>
+                    <div class="form-group">
+                      <label for="categoryTitle"><span class="fieldInfo">Choose Category:</span> </label>
+                        <select class="form-control" name="category" id="categoryTitle">
+                           <?php
+                               global $connectingDB;
+                               $sql = "SELECT id,title FROM category";
+                               $stmt = $connectingDB->query($sql);
+                               while($dataRows = $stmt->fetch()){
+                                    $id = $dataRows['id'];
+                                    $categoryName = $dataRows['title'];
+                               
+                            ?>
+                            <option><?php echo$categoryName; ?></option>
+                            <?php } ?>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <div class="custom-file">
+                         <input class="custom-file-input" type="file" name="image" id="imageSelect" value="">
+                         <label for="imageSelect" class="custom-file-label">Select Image</label>
+                       </div>
+                    </div>
+                    <div class="form-group">
+                      <label for="post"><span class="fieldInfo">Post :</span></label>
+                      <textarea class="form-control" name="postDescription" id="post" cols="30" rows="10"></textarea>
+                    </div>
+                    <div class="row">
+                       <div class="col-lg-6 mb-2">
+                         <a href="dashboard.php" class="btn btn-warning btn-block"><i class="fas fa-arrow-left"></i>Back To Dashboard</a>
+                       </div>
+                       <div class="col-lg-6 mb-2">
+                       <button type="submit" name="submit" class="btn btn-success btn-block">
+                         <i class="fas fa-check"></i>Publish
+                       </button>
+                    </div>
+                  </div>
+                </div>
+                </div>
+              </form>
+           </div>
+         </div>
+       </section>
+
+     <!-- The End area -->
+    
+
+   <!--   FOOTER -->
+     <footer class="bg-dark text-white">
+      <div class="container">
+            <div class="row">    <!--  row-all accros container, that kind class in bootstrap -->
+               <div class="col">
+                  <p class="lead text-center">Theme By Dinko Dugec | <span id="year"></span> All reight reserved</p> 
+              </div> 
+           </div>  
+      </div>
+    </footer>
+    <!--  END FOOTER -->
+
+  
+
+    <script
+      src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+      integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/popper.js@1.14.6/dist/umd/popper.min.js"
+      integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut"
+      crossorigin="anonymous"
+    ></script>
+    <script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@4.2.1/dist/js/bootstrap.min.js"
+      integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k"
+      crossorigin="anonymous"
+    ></script>
+
+    <script>
+      $('#year').text(new Date().getFullYear());
+    </script>
+
+  </body>
+</html>
+
+
+<!-- for a white navigation <nav class="navbar navbar-expand-lg navbar-light bg-light" -->
